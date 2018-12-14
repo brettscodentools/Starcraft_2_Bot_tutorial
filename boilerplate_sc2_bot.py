f2db8ab0c9dbca1e7a755c033f7720047f84a9b4
Brett Stephens <BStephensgame@gmail.com>
2018-12-14 04:40:40 -0800

    Initial Commit

---
+++ boilerplate_sc2_bot.py
@@ -0,0 +1,15 @@
+#! python3
+#shebang line^
+
+# Non learning branch for familiarity's sake
+
+#importing sc2 package
+import sc2
+from sc2 import run_game, maps, Race, Difficulty
+from sc2.player import Bot, Computer
+
+
+#this is my bot class to fill out with rules
+class kipBot(sc2.BotAI):
+    __init__():
+
